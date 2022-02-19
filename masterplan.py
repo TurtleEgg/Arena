0aa349e21374067a6ece4c2392aa02bc8b175ef4
TurtleEgg <jake-take@yandex.ru>
2022-02-19 17:28:04 +0300

    masterplan учтен showmatch, cyclotron обновлен шаг.

--- masterplan.py
+++ masterplan.py
@@ -7,3 +7,12 @@ tron.grind(120, dump_step=20)
 tron.export_champions(".dat")
 plot(tron.champ_scores); show()
 tron.showmatch()
+
+from cyclotron import Cyclotron
+from matplotlib.pyplot import plot, show
+tron = Cyclotron(num_champions=20, num_teams=400)
+tron.get_start_population()
+tron.grind_es(60, dump_step=10); tron.grind_cma_es(10)
+tron.export_champions_to_file("file.dat")
+plot(tron.champ_scores); show()
+tron.showmatch(1)
